7c5c15d9a2273904876a570fa2f4a4329e0e0967
kaushik <kaushik.paul755@gmail.com>
2023-10-25 14:31:35 +0530

    Make the producer high throughput

--- kafka-producer-wikimedia/src/main/java/com/example/kafka/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/example/kafka/WikimediaChangesProducer.java
@@ -29,6 +29,11 @@ public class WikimediaChangesProducer {
 //        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
 //        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
+        // Set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(1024 * 32));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         // Create producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
